0e36ad4e11c6e183e62605ac2c3e2c89bd9828c5
Lenka Černohorská <cernohorska.lc@gmail.com>
2023-03-11 23:04:59 +0100

    Přidán JS

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 console.log('funguju!');
+
+// KOSTKA
+/*
+Naklonujte si repozitář se stránkou, která zobrazuje hrací kostku. Doplňte do stránky JavaScriptový program, který zařídí, že když uživatel zmáčkne na stránce libovolnou klávesu, na kostce se zobrazí číslo o jedna vyšší. Po šestce zobrazte opět jedničku.
+
+Nápověda: Rozumným postupem je vytvořit si proměnnou side, která bude obsahovat číslo strany kostky. Tuto proměnnou můžete vždy při stisknutí klávesy navýšit o jedna a z této hodnoty zkonstruovat řetězec pro atribut src.
+*/
+
+let cislo = 0
+
+const diceGame = () => {
+    if (cislo < 6) {
+        cislo = cislo + 1
+        let side = document.querySelector('.dice')
+        side.src = `img/side${cislo}.svg`
+    } else {
+        cislo = 0
+        diceGame()
+    }
+}
+
+document.addEventListener('keydown', diceGame)
+
+
